5615aae227631786c5169b2393bf7e68263fc5be
Andrey Nesterkin <nesterkin.am@gmail.com>
2023-12-20 00:39:17 +0300

    refactor: add index for url table

--- migration/000001_init.up.sql
+++ migration/000001_init.up.sql
@@ -1,4 +1,11 @@
-CREATE TABLE IF NOT EXISTS url(id SERIAL PRIMARY KEY,
+BEGIN;
+
+CREATE TABLE url(id SERIAL PRIMARY KEY,
     short_url VARCHAR(255),
     original_url TEXT UNIQUE
 );
+
+CREATE INDEX idx_url_short 
+ON url(short_url);
+
+COMMIT;
