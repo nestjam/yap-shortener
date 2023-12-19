4ce38794764394829a78692e5b1c66fda03be49b
Andrey Nesterkin <nesterkin.am@gmail.com>
2023-12-20 00:46:24 +0300

    fix: add constraint to columns of url table

--- migration/000001_init.up.sql
+++ migration/000001_init.up.sql
@@ -1,8 +1,8 @@
 BEGIN;
 
 CREATE TABLE url(id SERIAL PRIMARY KEY,
-    short_url VARCHAR(255),
-    original_url TEXT UNIQUE
+    short_url VARCHAR(255) UNIQUE NOT NULL,
+    original_url TEXT UNIQUE NOT NULL
 );
 
 CREATE INDEX idx_url_short 
