daabb15597197aa300b743bc9845abd0a974c3eb
Andrey Nesterkin <nesterkin.am@gmail.com>
2023-12-21 18:22:31 +0300

    refactor: remove extra index on short_url

--- migration/000001_init.up.sql
+++ migration/000001_init.up.sql
@@ -1,11 +1,4 @@
-BEGIN;
-
 CREATE TABLE url(id SERIAL PRIMARY KEY,
     short_url VARCHAR(255) UNIQUE NOT NULL,
     original_url TEXT UNIQUE NOT NULL
 );
-
-CREATE INDEX idx_url_short 
-ON url(short_url);
-
-COMMIT;
