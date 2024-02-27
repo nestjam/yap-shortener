1a79a7ee8781d8f73cd7bd854ac6ebf5393ef570
Andrey Nesterkin <nesterkin.am@gmail.com>
2024-02-27 20:09:11 +0300

    docs: add example of expand URL

---
+++ internal/server/server_example_test.go
@@ -0,0 +1,34 @@
+package server
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/nestjam/yap-shortener/internal/domain"
+	"github.com/nestjam/yap-shortener/internal/persistance/inmemory"
+)
+
+func ExampleServer_ServeHTTP_expandURL() {
+	u := URLShortenerTest{
+		CreateDependencies: func() (domain.URLStore, Cleanup) {
+			return inmemory.New(), func() {}
+		},
+	}
+	urlStore, cleanup := u.CreateDependencies()
+	defer cleanup()
+	userID := domain.NewUserID()
+	pair := domain.URLPair{
+		ShortURL:    "EwHXdJfB",
+		OriginalURL: "https://practicum.yandex.ru/",
+	}
+	_ = urlStore.AddURL(context.Background(), pair, userID)
+	sut := New(urlStore, baseURL)
+
+	request := httptest.NewRequest(http.MethodGet, "/"+pair.ShortURL, nil)
+	request.Header.Set(contentTypeHeader, textPlain+"; charset=utf-8")
+
+	response := httptest.NewRecorder()
+
+	sut.ServeHTTP(response, request)
+}
